5938f0dd70d2d231d208f7f8fb9ac4c0067c4d0a
Daniels <ducktheprogrammer@gmail.com>
2026-07-06 15:53:57 +0300

    Add shrimple serial output to the kernel

--- kodols/arch/i386/main.cpp
+++ kodols/arch/i386/main.cpp
@@ -1,10 +1,22 @@
 // Kodols entrypoint :3
 
+#include "devices/serial.h"
 #include <stdint.h>
 
 extern "C" {
 
 extern void init_gdt(uint8_t* gdt_location);
 
-void main() {}
+void main() {
+  init_serial();
+  write_serial('h');
+  write_serial('e');
+  write_serial('l');
+  write_serial('l');
+  write_serial('o');
+  write_serial('r');
+  write_serial('l');
+  write_serial('d');
+  // :3
+}
 }
